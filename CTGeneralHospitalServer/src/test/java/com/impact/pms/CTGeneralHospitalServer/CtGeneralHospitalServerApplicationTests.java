0d9c01457348918853f44baadd0825cee8798744
Ankit <65398418+ankitpodey@users.noreply.github.com>
2021-05-21 19:13:10 +0530

    added file

---
+++ CTGeneralHospitalServer/src/test/java/com/impact/pms/CTGeneralHospitalServer/CtGeneralHospitalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impact.pms.CTGeneralHospitalServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtGeneralHospitalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
